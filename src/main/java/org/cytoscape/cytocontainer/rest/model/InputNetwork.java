f4c5feb532e4f06f03b41e7340a9bc5f362d1f56
Chris Churas <churas.camera@gmail.com>
2024-10-11 11:36:19 -0700

    limit format and model allowable values. also added edgelist format

--- src/main/java/org/cytoscape/cytocontainer/rest/model/InputNetwork.java
+++ src/main/java/org/cytoscape/cytocontainer/rest/model/InputNetwork.java
@@ -2,6 +2,8 @@ package org.cytoscape.cytocontainer.rest.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.Set;
+import org.cytoscape.cytocontainer.rest.model.exceptions.CytoContainerException;
 
 /**
  * Defines expected input for this service to be a network
@@ -12,7 +14,12 @@ public class InputNetwork {
 	public static final String NETWORK_MODEL = "network";
 	public static final String GRAPH_MODEL = "graph";
 	
+	public static final Set<String> MODEL_SET = Set.of(NETWORK_MODEL, GRAPH_MODEL);
+	
 	public static final String CX_FORMAT = "cx2";
+	public static final String EDGELIST_FORMAT = "edgelist";
+	public static final Set<String> FORMAT_SET = Set.of(CX_FORMAT, EDGELIST_FORMAT);
+	
 	private String _model;
 	private String _format;
 
@@ -35,17 +42,24 @@ public class InputNetwork {
 		return _model;
 	}
 
-	public void setModel(String model) {
+	public void setModel(String model) throws CytoContainerException {
+		if (model != null && !MODEL_SET.contains(model)){
+			throw new CytoContainerException(model + " is not a valid model");
+		}
 		this._model = model;
 	}
 
 	@Schema(description="Denotes format of returned data. '" + InputNetwork.CX_FORMAT + "' returns CX2 format.",
-			allowableValues = {InputNetwork.CX_FORMAT}, example=InputNetwork.CX_FORMAT)
+			allowableValues = {InputNetwork.CX_FORMAT,
+			                   InputNetwork.EDGELIST_FORMAT}, example=InputNetwork.CX_FORMAT)
 	public String getFormat() {
 		return _format;
 	}
 
-	public void setFormat(String format) {
+	public void setFormat(String format) throws CytoContainerException {
+		if (format != null && !FORMAT_SET.contains(format)){
+			throw new CytoContainerException(format + " is not a valid format");
+		}
 		this._format = format;
 	}
 	
