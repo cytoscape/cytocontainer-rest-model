20e00b4289d68c03eeacdd2935a6801138829bb6
Chris Churas <churas.camera@gmail.com>
2024-10-11 11:35:53 -0700

    limited valid data types and added allowmultipleselection

--- src/main/java/org/cytoscape/cytocontainer/rest/model/InputColumn.java
+++ src/main/java/org/cytoscape/cytocontainer/rest/model/InputColumn.java
@@ -2,6 +2,7 @@ package org.cytoscape.cytocontainer.rest.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.v3.oas.annotations.media.Schema;
+import org.cytoscape.cytocontainer.rest.model.exceptions.CytoContainerException;
 
 /**
  * Defines expected input data to service to be either
@@ -11,9 +12,12 @@ import io.swagger.v3.oas.annotations.media.Schema;
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class InputColumn {
+	
+	
 	private String _name;
 	private String _description;
 	private String _dataType;
+	private boolean _allowMultipleSelection;
 	private String _defaultColumnName;
 	private String _columnName;
 	
@@ -29,6 +33,7 @@ public class InputColumn {
 		_dataType = src.getDataType();
 		_defaultColumnName = src.getDefaultColumnName();
 		_columnName = src.getColumnName();
+		_allowMultipleSelection = src.getAllowMultipleSelection();
 	}
 
 	@Schema(description="The label text of the dropdown list. It will also be used as the column name in the generated data block.")
@@ -49,14 +54,32 @@ public class InputColumn {
 		this._description = description;
 	}
 
-	@Schema(description="Data type of the attribute. When defined, Cytoscape Web uses it to filter the dropdown list. If omitted, the dropdown will contain all columns.")
+	@Schema(description="Data type of the attribute. When defined, Cytoscape Web uses it to filter the dropdown list. If omitted, the dropdown will contain all columns.",
+	        allowableValues = {CXDataType.LIST_OF, CXDataType.NUMBER, CXDataType.WHOLE_NUMBER,CXDataType.LIST_OF_NUMBER, CXDataType.LIST_OF_WHOLE_NUMBER, CXDataType.STRING_TYPE, CXDataType.LONG_TYPE, CXDataType.INTEGER_TYPE, CXDataType.DOUBLE_TYPE, CXDataType.BOOLEAN_TYPE,
+				CXDataType.LIST_OF_STRING_TYPE, CXDataType.LIST_OF_LONG_TYPE, CXDataType.LIST_OF_INTEGER_TYPE, CXDataType.LIST_OF_DOUBLE_TYPE, CXDataType.LIST_OF_BOOLEAN_TYPE
+			})
 	public String getDataType() {
 		return _dataType;
 	}
 
-	public void setDataType(String dataType) {
+	public void setDataType(String dataType) throws CytoContainerException {
+		if (dataType != null && !CXDataType.COLUMN_FILTER_SET.contains(dataType)){
+			throw new CytoContainerException(dataType + " is not a valid dataType");
+		}
 		this._dataType = dataType;
 	}
+
+	@Schema(description="When set to true, all selected columns in the dropdown will be used to serialize the data, otherwise only one column can be selected")
+	public boolean getAllowMultipleSelection() {
+		return _allowMultipleSelection;
+	}
+
+	
+	public void setAllowMultipleSelection(boolean allowMultipleSelection) {
+		this._allowMultipleSelection = allowMultipleSelection;
+	}
+	
+	
 
 	@Schema(description="(Optional) The default selection in the column name dropdown. If the column specified in field doesn't exist in node or edge table, no selection will be make.")
 	public String getDefaultColumnName() {
